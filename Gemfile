1847a152310f48ca59fa2d8baa91ce0f68786d7a
Nathaniel Trujillo <nathaniel.trujillo@tamu.edu>
2022-10-11 18:13:00 -0500

    added default accounts

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'omniauth'
 gem 'omniauth-google-oauth2'
 gem 'omniauth-rails_csrf_protection' , '~> 1.0'
 gem 'pry'
+gem 'seed_dump'
 
 group :development, :test do
      # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -68,12 +68,6 @@ ActiveRecord::Schema.define(version: 2022_10_11_215027) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "uid"
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
   create_table "vendors", force: :cascade do |t|
