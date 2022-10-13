f401ee1e3b3f6c545cf71220e9ea68bba7e1c00b
shivhouston <95387363+shivhouston@users.noreply.github.com>
2022-10-12 22:36:25 -0500

    working user unit tests and broken in progress login integration tests

--- spec/feature/login_spec.rb
+++ spec/feature/login_spec.rb
@@ -3,21 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe('Login user', type: :feature) do
+
      it 'existing user' do
           visit login_path
-          fill_in 'email', with: 'shivhoustontx@gmail.com'
-          fill_in 'password', with: 'TempPassword'
-          click_on 'Log In'
-          expect(page).to(have_content('User Home Page'))
-     end
-end
+          click_on 'Google'
+          click_on 'Use another account'
+          fill_in 'Email or phone', with: 'secbrsuser@gmail.com'
+          click_on 'Next'
+          fill_in 'Enter your password', with: '#SECbrs23'
+          click_on 'Next'
+          click_on 'Profile'
+          expect(page).to(have_content('Email: secbrsuser@gmail.com'))
+          expect(page).to(have_content('Permission type: user'))
 
-RSpec.describe('Login user', type: :feature) do
-     it 'nonexisting user' do
-          visit login_path
-          fill_in 'email', with: 'xyz'
-          fill_in 'password', with: 'xyz'
-          click_on 'Log In'
-          expect(page).to(have_content('Something went wrong! Make sure your username and password are correct.'))
      end
+
+
 end
