ce76e5ce4c2789ef4aa36846f7bccf88c7708dc2
Julia Teleki <juliateleki@tamu.edu>
2022-11-11 23:47:55 -0600

    fixed bootstrap & ruby incompatibility with removing db objects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,11 +13,14 @@ module ApplicationHelper
           @current_user ||= User.find_by(id: session[:user_id]) if !!session[:user_id]
      end
 
-     def active_class(path)
-          if request.path == path
-               return 'active'
-          else
-               return ''
-          end
+     def flash_class(level)
+          bootstrap_alert_class = {
+            "success" => "alert-success",
+            "error" => "alert-danger",
+            "notice" => "alert-info",
+            "alert" => "alert-danger",
+            "warn" => "alert-warning"
+          }
+          bootstrap_alert_class[level]
      end
 end
