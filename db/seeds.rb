efa15bd0dd3c6bd4c8c74e88bafd9ce9e59a8da4
Systems Administrator <sysadmin@sec.tamu.edu>
2023-02-21 11:09:13 -0600

    redeploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 vendor = Vendor.create!(
      {
           vendor_name: 'no vendor',
@@ -112,7 +111,7 @@ defaultUser = User.create!(
           zip_code: '77845',
           uin: 123,
           email: 'secbrsuser@gmail.com',
-          phone_number: '123-456-7890',
+          phone_number: '123-456-7899',
           committee_id: committee01.id,
           permission_type: 'admin',
           uid: '100003231053752770743'
