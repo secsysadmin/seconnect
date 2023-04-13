1eb49ab56c815285bfb52adfc8835ae1e6727849
Systems Administrator <sysadmin@sec.tamu.edu>
2023-04-13 10:24:56 -0500

    removed file from requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,12 +1,8 @@
-# frozen_string_literal: true
-
 class Request < ApplicationRecord
      belongs_to :user
      belongs_to :budget_subcategory
      belongs_to :vendor
-     has_one_attached :file
      validates :status, presence: true
-     validates :file, presence: true
 end
 
 def to_s
