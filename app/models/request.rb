d672a5ca874ba3eef99643efdc4efd3e11b63ada
Systems Administrator <sysadmin@sec.tamu.edu>
2023-04-18 10:30:51 -0500

    Vendor required for company reimbursements

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,9 +1,10 @@
 class Request < ApplicationRecord
      belongs_to :user
      belongs_to :budget_subcategory
-     belongs_to :vendor
+     belongs_to :vendor, optional: true
      validates :status, presence: true
-end
+   end
+   
 
 def to_s
      status
