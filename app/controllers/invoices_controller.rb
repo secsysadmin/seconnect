370e6e1c6bddd4db4252a61bea364e9542c92ea1
Systems Administrator <sysadmin@sec.tamu.edu>
2023-04-18 15:15:46 -0500

    invoices controller change

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,100 +2,96 @@
 
 class InvoicesController < ApplicationController
      before_action :set_invoice, only: %i[show edit update destroy]
-
+     before_action :require_user
+   
      # GET /invoices or /invoices.json
      def index
-          @invoices = Invoice.all
+       @invoices = Invoice.all
      end
-
+   
      # GET /invoices/1 or /invoices/1.json
-     def show
-          @user = User.find(session[:user_id])
-          if session[:user_id]
-            @current_user = User.find(session[:user_id])
-            if @current_user.permission_type == 'user' || @current_user.permission_type == 'admin'
-              @invoice = Invoice.find(params[:id])
-            else
-              redirect_to(root_url) and return
-            end
-          else
-            redirect_to(root_url) and return
-          end
-        end
-        
-
+     def show; end
+   
      # GET /invoices/new
      def new
-          @invoice = Invoice.new
-          @invoice.items << Item.new
-          @invoice.items << Item.new
-          @invoice.items << Item.new
-          @invoice.items << Item.new
-          @invoice.items << Item.new
+       @invoice = Invoice.new
+       @invoice.items << Item.new
+       @invoice.items << Item.new
+       @invoice.items << Item.new
+       @invoice.items << Item.new
+       @invoice.items << Item.new
      end
-
+   
      # GET /invoices/1/edit
      def edit; end
-
+   
      # POST /invoices or /invoices.json
      def create
-          @invoice = Invoice.new(invoice_params)
-         
-          respond_to do |format|
-               if @invoice.save
-                    format.html do
-                         redirect_to(invoice_url(@invoice),
-                                     notice: 'Invoice was successfully created.'
-                                    )
-                    end
-                    format.json { render(:show, status: :created, location: @invoice) }
-               else
-                    format.html { render(:new, status: :unprocessable_entity) }
-                    format.json { render(json: @invoice.errors, status: :unprocessable_entity) }
-               end
-          end
+       @invoice = Invoice.new(invoice_params)
+   
+       respond_to do |format|
+         if @invoice.save
+           format.html do
+             redirect_to(invoice_url(@invoice),
+                         notice: 'Invoice was successfully created.'
+                        )
+           end
+           format.json { render(:show, status: :created, location: @invoice) }
+         else
+           format.html { render(:new, status: :unprocessable_entity) }
+           format.json { render(json: @invoice.errors, status: :unprocessable_entity) }
+         end
+       end
      end
-
+   
      # PATCH/PUT /invoices/1 or /invoices/1.json
      def update
-          respond_to do |format|
-               if @invoice.update(invoice_params)
-                    format.html do
-                         redirect_to(invoice_url(@invoice),
-                                     notice: 'Invoice was successfully updated.'
-                                    )
-                    end
-                    format.json { render(:show, status: :ok, location: @invoice) }
-               else
-                    format.html { render(:edit, status: :unprocessable_entity) }
-                    format.json { render(json: @invoice.errors, status: :unprocessable_entity) }
-               end
-          end
+       respond_to do |format|
+         if @invoice.update(invoice_params)
+           format.html do
+             redirect_to(invoice_url(@invoice),
+                         notice: 'Invoice was successfully updated.'
+                        )
+           end
+           format.json { render(:show, status: :ok, location: @invoice) }
+         else
+           format.html { render(:edit, status: :unprocessable_entity) }
+           format.json { render(json: @invoice.errors, status: :unprocessable_entity) }
+         end
+       end
      end
-
+   
      # DELETE /invoices/1 or /invoices/1.json
      def destroy
-          @invoice.destroy!
-          
-          respond_to do |format|
-               format.html do
-                    redirect_to(invoices_url, notice: 'Invoice was successfully deleted.')
-               end
-               format.json { head(:no_content) }
-          end
+       @invoice.destroy!
+   
+       respond_to do |format|
+         format.html do
+           redirect_to(invoices_url, notice: 'Invoice was successfully deleted.')
+         end
+         format.json { head(:no_content) }
+       end
      end
-
+   
      private
-
+   
      # Use callbacks to share common setup or constraints between actions.
      def set_invoice
-          @invoice = Invoice.find(params[:id])
+       @invoice = Invoice.find(params[:id])
      end
-
+   
      # Only allow a list of trusted parameters through.
      def invoice_params
-          params.require(:invoice).permit(:vendor_id, :tax_id_number, :address, :city,
-                                          :state, :zip, :payment_method, :notes, :travel_password, items_attributes:[:items_purchased, :budget, :category, :subcategory, :taxcategory, :gift, :cost]
-          )
+       params.require(:invoice).permit(:vendor_id, :tax_id_number, :address, :city,
+                                       :state, :zip, :payment_method, :notes, :travel_password, items_attributes:[:items_purchased, :budget, :category, :subcategory, :taxcategory, :gift, :cost]
+       )
      end
-end
+   
+     # Add the require_user method
+     def require_user
+       unless session[:user_id] && (User.find(session[:user_id]).permission_type == 'user' || User.find(session[:user_id]).permission_type == 'admin')
+         redirect_to(root_url) and return
+       end
+     end
+   end
+   
