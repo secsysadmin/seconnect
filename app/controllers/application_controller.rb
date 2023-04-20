5aa6b25dbcd6defecb0bea1512fa510b85ee64e4
Systems Administrator <sysadmin@sec.tamu.edu>
2023-04-20 17:36:19 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
-end
+    helper_method :current_user
+  
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+  
