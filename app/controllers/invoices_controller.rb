765bb362880b1482fbe08d213b2375466d84db7e
Systems Administrator <sysadmin@sec.tamu.edu>
2023-04-20 10:20:57 -0500

    added categories to invoice controller new function

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -15,6 +15,8 @@ class InvoicesController < ApplicationController
      # GET /invoices/new
      def new
        @invoice = Invoice.new
+       @categories = Category.order(:name)
+       @subcategories = Subcategory.order(:name)
        @invoice.items << Item.new
        @invoice.items << Item.new
        @invoice.items << Item.new
